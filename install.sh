cf2d0db9b9472ea89f56344c5f769b890670e432
kobayash1 <kobayashi@teamspooky.com>
2018-04-17 12:02:05 -0500

    rofi config and fix the install script

--- install.sh
+++ install.sh
@@ -1,19 +1,20 @@
 #!/bin/sh
 
-ln -sv ~/.dotfiles/.bashrc ~
-ln -sv ~/.dotfiles/.bash_profile ~
-ln -sv ~/.dotfiles/.conkyrc ~
-ln -sv ~/.dotfiles/.vimrc ~
-ln -sv ~/.dotfiles/.xbindkeysrc ~
-ln -sv ~/.dotfiles/.config/compton/* ~/.config/compton/
-ln -sv ~/.dotfiles/.config/i3/* ~/.config/i3/
-ln -sv ~/.dotfiles/.config/mpd/* ~/.config/mpd/
-ln -sv ~/.dotfiles/.config/mpv/* ~/.config/mpv/
-ln -sv ~/.dotfiles/.config/polybar/* ~/.config/polybar/
-ln -sv ~/.dotfiles/.config/termite/* ~/.config/termite/
-ln -sv ~/.dotfiles/.config/redshift.conf ~/.config/
-ln -sv ~/.dotfiles/.irssi/* ~/.irssi/
-cp ~/.dotfiles/.config/gsimplecal/* ~/.config/gsimplecal/
-cp ~/.dotfiles/.config/mpdnotify.conf ~/.config/
-cp ~/.dotfiles/.config/dunst/dunstrc ~/.config/dunst/
-cp -r ~/.dotfiles/.mpdcron ~/
+ln -sv ~/.dotfiles-x201/.bashrc ~
+ln -sv ~/.dotfiles-x201/.bash_profile ~
+ln -sv ~/.dotfiles-x201/.conkyrc ~
+ln -sv ~/.dotfiles-x201/.vimrc ~
+ln -sv ~/.dotfiles-x201/.xbindkeysrc ~
+ln -sv ~/.dotfiles-x201/.config/compton/* ~/.config/compton/
+ln -sv ~/.dotfiles-x201/.config/i3/* ~/.config/i3/
+ln -sv ~/.dotfiles-x201/.config/mpd/* ~/.config/mpd/
+ln -sv ~/.dotfiles-x201/.config/mpv/* ~/.config/mpv/
+ln -sv ~/.dotfiles-x201/.config/polybar/* ~/.config/polybar/
+ln -sv ~/.dotfiles-x201/.config/termite/* ~/.config/termite/
+ln -sv ~/.dotfiles-x201/.config/redshift.conf ~/.config/
+ln -sv ~/.dotfiles-x201/.config/rofi/* ~/.config/rofi/
+ln -sv ~/.dotfiles-x201/.irssi/* ~/.irssi/
+cp ~/.dotfiles-x201/.config/gsimplecal/* ~/.config/gsimplecal/
+cp ~/.dotfiles-x201/.config/mpdnotify.conf ~/.config/
+cp ~/.dotfiles-x201/.config/dunst/dunstrc ~/.config/dunst/
+cp -r ~/.dotfiles-x201/.mpdcron ~/
